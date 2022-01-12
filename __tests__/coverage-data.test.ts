100793eef0c7d747d90bc9a58232f732827f5123
Benedikt Weber <benedikt.web23@gmail.com>
2022-01-13 00:00:46 +0100

    Lint tests

--- __tests__/coverage-data.test.ts
+++ __tests__/coverage-data.test.ts
@@ -2,10 +2,10 @@ import {expect, test} from '@jest/globals'
 import {CoverageData} from '../src/coverage-data'
 
 test('build markdown table', async () => {
-  let json =
+  const json =
     '{"src/foo.rs":{"total":2,"with_docs":2,"total_examples":2,"with_examples":0},"src/lib.rs":{"total":3,"with_docs":2,"total_examples":3,"with_examples":1}}'
-  let coverageData = new CoverageData(json)
-  let expectedTable = [
+  const coverageData = new CoverageData(json)
+  const expectedTable = [
     ['File', 'Documented', 'Percentage', 'Examples', 'Percentage'],
     ['src/foo.rs', '2', '100.00%', '0', '0.00%'],
     ['src/lib.rs', '2', '66.67%', '1', '33.33%'],
